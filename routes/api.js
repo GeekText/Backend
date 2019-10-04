153a4d8f178883b33469702d84e4a00645eb8d71
Help <pczerokey@gmail.com>
2019-10-04 18:27:19 -0400

    getting data beautifully

--- routes/api.js
+++ routes/api.js
@@ -4,18 +4,41 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://viewer:teampass@cluster0-2eofd.mongodb.net/bookstoreDB?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const assert = require('assert');
+let data
 
 /* GET Book listings */
 router.get('/', function(req, res, next) {
 
-    client.connect(function(err) {
+    client.connect(function(err, db) {
+
+        if(err) 
+        {
+            console.log(err);
+            process.exit(0);
+        }
+
         assert.equal(null, err);
         console.log("Connected correctly to server");
       
+        let DB = db.db('bookstoreDB')
+        let collection = DB.collection('bookCOLLECTION');
+
+        collection.find().toArray(( err, results) => {
+
+            if(err) 
+            {
+                console.log(err);
+                process.exit(0);
+            }
+            data = results
+            console.log(results)
+        })
+
+        db.close();
         client.close();
       });
 
-      res.send("KMS")
+    res.send(data);
 
 });
 
